9577ba82388d1958758955a234e723132fe14a54
JacobLRL <JLittle@spartaglobal.com>
2019-01-21 11:28:41 +0000

    added css so it looks like minesweeper boards, and some JS to create the board

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", function(){
-    
+document.addEventListener("DOMContentLoaded", function () {
+    let row = 8;
+    let column = 8;
+    let board = document.getElementById("board");
+
+    function createGrid() {
+        // clearChildren();
+        for (let j = 0; j < row; j++) {
+            const row1 = document.createElement("div");
+            row1.setAttribute("class", "row");
+            for (let i = 0; i < column; i++) {
+                const square = document.createElement("div");
+                square.setAttribute("class", "square");
+                square.setAttribute("tabindex", "-1");
+                row1.appendChild(square);
+            }
+            board.appendChild(row1);
+        }
+        let squares = document.getElementsByClassName("square");
+        for (let i = 0; i < squares.length; i++) {
+            squares[i].addEventListener("keydown", function (e) {
+                e.target.setAttribute("class", "square-clicked");
+            })
+        }
+    }
+
+    createGrid();
 }); // end of dom
